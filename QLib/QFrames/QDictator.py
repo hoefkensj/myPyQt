0a01a0552ca94170a1d55362881635fe530bb34a
hoefkensj <hoefkens.j@gmail.com>
2022-12-10 18:34:11 +0100

    Update workspace.xml, Config.cpython-310.pyc, and 17 more files...

--- QLib/QFrames/QDictator.py
+++ QLib/QFrames/QDictator.py
@@ -6,60 +6,90 @@ from QLib.QModules import QHIncDec,QHSearch, QEditProp
 from QLib.QElements import QTree,QTextButton
 from QLib.QBases import QModule
 
-def mod_TreeCtl(name):
-	w=QModule.make(name)
-	w['Elements']|=gnr.Element(QHIncDec.make('ColEx',wh=[20,20]))
-	w['Elements']|=gnr.Element(QHSearch.make('TreeSearch',))
-	w=w['Fnx']['Run'](w)
-	sfn=gnr.Short(w,'Con')
-	w['Con']['+']=sfn['ColEx']['Inc']
-	w['Con']['-']=sfn['ColEx']['Dec']
-	return w
-def mod_AppCtl(name):
 
-	w=QModule.make(name)
-	w['Elements']|= gnr.Element(QTextButton.make('Update',pol='E.P',))
-	w['Elements']|= gnr.Element(QTextButton.make('Print',pol='E.P',))
-	w['Elements']|= gnr.Element(QTextButton.make('Exit',pol='E.P',))
-	w=w['Fnx']['Run'](w)
-	sfn=gnr.Short(w,'Con','clicked')
-	w['Con']['Update']=sfn['Update']
-	w['Con']['Print']=sfn['Print']
-	w['Con']['Exit']=sfn['Exit']
-	w['Con']['Exit'](sys.exit)
-	return w
+def QDictator(**k):
+	def Elements(m):
+		def mod_TreeCtl(name):
+			w=QModule.make(name)
+			w['Elements']|=gnr.Element(QHIncDec.make('ColEx',wh=[20,20]))
+			w['Elements']|=gnr.Element(QHSearch.make('TreeSearch',))
+			w=w['Fnx']['Run'](w)
+			sfn=gnr.Short(w,'Con')
+			w['Con']['+']=sfn['ColEx']['Inc']
+			w['Con']['-']=sfn['ColEx']['Dec']
+			return w
+		def mod_AppCtl(name):
+
+			w=QModule.make(name)
+			w['Elements']|= gnr.Element(QTextButton.make('Update',pol='E.P',))
+			w['Elements']|= gnr.Element(QTextButton.make('Print',pol='E.P',))
+			w['Elements']|= gnr.Element(QTextButton.make('Exit',pol='E.P',))
+			w=w['Fnx']['Run'](w)
+			sfn=gnr.Short(w,'Con','clicked')
+			w['Con']['Update']=sfn['Update']
+			w['Con']['Print']=sfn['Print']
+			w['Con']['Exit']=sfn['Exit']
+			w['Con']['Exit'](sys.exit)
+			return w
+		# GUI['Elements']|=gnr.Element(component)
+		m['Elements']	|= gnr.Element(QTree.make('Tree',cols=7,))##hidecols=[2,3,4,5,6]
+		m['Elements']	|= gnr.Element(mod_TreeCtl('TreeCtl'))
+		m['Elements']	|= gnr.Element(QEditProp.make('Key',))
+		m['Elements']	|= gnr.Element(QEditProp.make('Val',))
+		m['Elements']	|= gnr.Element(mod_AppCtl('AppCtl'))
+	def Fnx(m):
+		def Select(*a,**k):
+			sFnx=gnr.Short(MAIN,'Fnx')
+			txtBox = [sFnx['Key']['txtText'], sFnx['Val']['txtText']]
+			def select(data):
+				for idx, txtbox in zip([0, 1, 2], txtBox):
+					txtbox(data.text(idx))
+			return select
+		def AddDict(m):
+			def adddict(**k):
+				#use adddict(NAME=DICT)
+				m['Elements']['trw_Tree']['Fnx']['Add'](**k)
+			return adddict
+		def Allign(m):
+			s=gnr.Short(m,'Fnx')
+			wMax = max(s['Key']['wLbl'](), s['Val']['wLbl']())+10
+			s['Key']['Allign'](wMax)
+			s['Val']['Allign'](wMax)
+		m['Fnx']={}
+		m['Fnx']['Select']=Select
+		m['Fnx']['AddDict']=AddDict(m)
+		m['Fnx']['Allign']=Allign
+		return m
+	def Con(m):
+		select=Select(m)
+		sCon=gnr.Short(m,'Con')
+		sFnx=gnr.Short(m,'Fnx')
+		sCon['TreeCtl']['+'](sFnx['Tree']['Mtd']['expandAll'])
+		sCon['TreeCtl']['-'](sFnx['Tree']['Mtd']['collapseAll'])
+		sCon['AppCtl']['Print'](m['Elements']['trw_Tree']['Fnx']['PrintTree'])
+		sCon['AppCtl']['Update'](m['Elements']['trw_Tree']['Fnx']['Update'])
+		sCon['Tree']['Item'](select)
+		return m
 
 
-def ADD(MAIN):
-		MAIN['Elements']['trw_Tree']['Fnx']['Add'](GUI=GUI)
-def Allign(MAIN):
-	s=gnr.Short(MAIN,'Fnx')
-	wMax = max(s['Key']['wLbl'](), s['Val']['wLbl']())+10
+	GUI=gui.make('Main')
+	MAIN=GUI['Main']
+	MAIN=Fnx(MAIN)
+	MAIN=Con(MAIN)
+	GUI['Fnx']['Main']()
 
-	s['Key']['Allign'](wMax)
-	s['Val']['Allign'](wMax)
-GUI=gui.make('Main')
-MAIN=GUI['Main']
-# GUI['Elements']|=gnr.Element(component)
-MAIN['Elements']|= gnr.Element(QTree.make('Tree',cols=7,hidecols=[2,3,4,5,6]))
-MAIN['Elements']|= gnr.Element(mod_TreeCtl('TreeCtl'))
-MAIN['Elements']|= gnr.Element(QEditProp.make('Key',))
-MAIN['Elements']|= gnr.Element(QEditProp.make('Val',))
-MAIN['Elements']|= gnr.Element(mod_AppCtl('AppCtl'))
 
-sCon=gnr.Short(MAIN,'Con')
-sFnx=gnr.Short(MAIN,'Fnx')
-sCon['TreeCtl']['+'](sFnx['Tree']['Mtd']['expandAll'])
-sCon['TreeCtl']['-'](sFnx['Tree']['Mtd']['collapseAll'])
-
-# sCon['AppCtl']['Update']()
-ADD(MAIN)
-sCon['AppCtl']['Print'](MAIN['Elements']['trw_Tree']['Fnx']['PrintTree'])
-sCon['AppCtl']['Update'](MAIN['Elements']['trw_Tree']['Fnx']['Update'])
-# GUI['Main']=GUI['Main']['Fnx']['Run'](GUI['Main'])
-GUI['Fnx']['Main']()
+addDict(MAIN)
 Allign(MAIN)
 GUI['Run'](GUI)
 
 print('After')
+
+		# def saveDialog():
+		# 	def saveDialog():
+		# 		Path, Type =QtWidgets.QFileDialog.getSaveFileName(QtWidgets.QWidget(),"Save As","","All Files (*)")
+		# 		return Path
+		# 	return saveDialog()
+
+
 
